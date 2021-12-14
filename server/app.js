713a8f3f60a9eb6089dce14b6213b87fb8d8fa34
doshrout <doshrout@ccstenn.com>
2021-12-14 17:34:54 -0600

    added connection to mlab mongodb database

--- server/app.js
+++ server/app.js
@@ -1,8 +1,15 @@
 const express = require('express');
 const { graphqlHTTP } = require('express-graphql');
 const schema = require('./schema/schema');
+const mongoose = require('mongoose');
 
 const app = express();
+
+// connect to mlab db
+mongoose.connect('mongodb+srv://dshrout:02Riley15!@gql-ninja.xdesm.mongodb.net/gql-ninja?retryWrites=true&w=majority');
+mongoose.connection.once('open', () => {
+    console.log('connected to the database');
+})
 
 app.use('/graphql', graphqlHTTP({
     schema,
